1a1232f04ef2dda2483e435a5b8e2798a87a1ec1
jmcshane <jmcshan1@gmail.com>
2017-08-31 20:17:42 -0500

    Adding something

--- src/main/java/com/example/scanthis/ScanThisApplication.java
+++ src/main/java/com/example/scanthis/ScanThisApplication.java
@@ -2,9 +2,18 @@ package com.example.scanthis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ScanThisApplication {
+
+	private static final String HELLO_WORLD = "Hello world!";
+	@GetMapping("/")
+	public String helloWorld() {
+		return HELLO_WORLD;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScanThisApplication.class, args);
